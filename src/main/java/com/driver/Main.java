f2a3492e49aee1f38cd1b655f72f236f87a81697
sanvu08 <sarveshkr1308@gmail.com>
2023-01-13 14:29:14 +0530

    Update 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) {
         A a = new A();
         B b = new B();
-        a.meth();
+        //a.meth();
         b.meth();
-//        System.out.println(a.meth());
-//        System.out.println(b.meth());
     }
 }
 
