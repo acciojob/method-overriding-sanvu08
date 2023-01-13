84c13b864a6728af883aa05356f0f993f989e986
sanvu08 <sarveshkr1308@gmail.com>
2023-01-13 14:19:43 +0530

    First ans of override function

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        a.meth();
+        b.meth();
+//        System.out.println(a.meth());
+//        System.out.println(b.meth());
+    }
 }
